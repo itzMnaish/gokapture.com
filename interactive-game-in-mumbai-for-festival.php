dfb64a50fed9e40ef426c0a7fa93159134d8edbd
manish kumar <techno8427@gmail.com>
2023-11-22 15:03:09 +0530

    init v2

---
+++ interactive-game-in-mumbai-for-festival.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            }); var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5KZC2ZL');</script>
+    <!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="description"
+        content="Interactive Game in Mumbai, Interactive Game for Festival, Best choice for sponsoring a festival, Ideas for promotions in festival">
+    <meta name="keywords" content="Interactive Game in Mumbai, interactive games, interactive game vending machine  
+">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags-->
+    <!-- Title-->
+    <title>360 Degree Spin Booth in Bangalore for Corporate Event
+    </title>
+    <!-- Favicon-->
+    <link rel="icon" href="img/gokapture/favicon.png">
+    <!-- Core Stylesheet-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/legcy/legcy-blog.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-098362XVT9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-098362XVT9');
+    </script>
+
+
+    <!--marketing team-->
+    <!-- Meta Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return; n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+            n.queue = []; t = b.createElement(e); t.async = !0;
+            t.src = v; s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '323335196628477');
+        fbq('track', 'PageView');
+    </script>
+    <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=323335196628477&ev=PageView&noscript=1" />
+    <!-- End Meta Pixel Code -->
+    <!---->
+
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KZC2ZL" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <!-- Preloader
+    <div class="preloader" id="preloader">
+      <div class="spinner-grow text-light" role="status"><span class="sr-only">Loading...</span></div>
+    </div>-->
+    <!-- Header Area-->
+    <?php include("tempnavbar.php"); ?>
+    <!-- Scroll Indicator-->
+    <div id="scrollIndicator"></div>
+
+    <!-- content goes here -->
+    <main class="main-casestudy-container">
+        <header class="casestudy-header">
+            <h1>WOW Skin Science Wows the Crowd at Nykaaland 2023 in Mumbai with Interactive Game Vending Machine
+            </h1>
+            <img class="full-width"
+                src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaAw_w5iWMAtmJMGj-uV9E8enzUXhkYS4qhWP5aZjrdx2EnQEo56_UC1RYJMLjwKw0EgbKqngtQ4Riay9Wg6FyBbAqDM=w1920-h892"
+                alt="360 Degree Spin Booth in Bangalore for Corporate Event">
+        </header>
+        <section>
+
+            <b>Video : </b>
+            </p>
+            <div class="center">
+                <video width="90%" controls>
+                    <source src="https://www.gokapture.com/video/cs/tata_1.mp4" type="video/mp4">
+                    Your browser does not support the video .
+                </video>
+            </div>
+            <p>
+                With household names such as Katrina Kaif rocking the stage, a crowd of a whopping 15,000, and over 80
+                featuring brands, the premier edition of Nykaaland 2023, help at Royal Western India Turf Club,
+                Mahalaxmi Race Course on November 4 and 5, 2023, was a resplendent success.
+            </p>
+            <p>
+                Having cemented its reputation as India’s leading online beauty shopping platform, <a
+                    href="https://www.nykaa.com/">Nykaa</a>Nykaa has expanded rapidly since its inception in 2012,
+                celebrating women and empowering them with its immensity of choices. Debuting in 2023, <a
+                    href=" https://www.nykaa.com/beauty-blog/nykaaland-tickets-are-live-book-your-spot-at-indias-first-ever-lifestyle-and-beauty-festival/">Nykaaland</a>
+                is the first festival of its kind in its home ground. Presenting itself as India’s first ever lifestyle
+                and beauty festival, the occasion was a meeting hub for the beauty world’s brightest influencers,
+                industry experts such as Mario Dedivanovic, Kim Kardashian’s makeup artist, and fashion connoisseurs.
+                The two-day gala was held in collaboration with <a href="https://in.bookmyshow.com/explore/events"></a>
+                BookMyShow LIVE and powered by <a
+                    href="https://www.rupay.co.in/our-cards/rupay-credit/explore-the-credit-card-range">RuPay
+                    Credit</a>, and featured numerous interactive zones, sampling and makeover stations, 12 carefully
+                curated masterclasses, music performances, panel discussions and more. Many Bollywood darlings greeted
+                the audience, and the event also served the stage for new launches and warm receptions to introductions
+                such as actress Kriti Sanon’s own brand <a
+                    href="https://letshyphen.com/collections/all?utm_source=google&utm_medium=cpc&utm_campaign=VM_Search_Brand_RLSA_24Aug%2723&utm_term=Kriti_Sanon&gclid=CjwKCAiAx_GqBhBQEiwAlDNAZr_JJDsy-yKj7GhIPC78IMtN-22PMIYJKU5jXPchMgeuVmrpuKaxTxoCcfcQAvD_BwE">Hyphen</a>.
+            </p>
+            <p>
+                A prominent participant at the event was WOW, the Bangalore-based skin care company that sought
+                GoKapture to create an unforgettable booth experience for its visitors at Nykaaland. <a
+                    href="https://www.buywow.in/">WOW Skin Science</a> is a home-grown brand, known particularly for its
+                sprawling portfolio of products that are cruelty-free, rid of harsh chemicals and closer to nature.
+            </p>
+            <p>This meeting proved a fruitful venture, as the brand’s generous gift-giving was transformed through
+                GoKapture’s innovation into an addictive interactive game in Mumbai that was prepared to entertain a
+                crowd of hundreds. The team at <a href="https://www.gokapture.com/">GoKapture</a> understood the
+                festival's ecosystem of rolling out samples and appreciative gifts, as well as the need to create a game
+                booth for WOW that would make it stand apart. Thus, we came up with the <b>Interactive Game Vending
+                    Machine</b> that was installed at the event as an incredibly eye-catching attraction that kept the
+                WOW station alight with the curiosity and enthusiasm of the visitors.
+                Video:
+            </p>
+            <p>
+                To boost the brand’s visibility in a refurbished manner and garner authentic engagement, we laid out the
+                groundwork for a fun tapping game that would be adored by all kinds of audiences. The key objectives of
+                value interaction, longer time spent at the booth, and social media buzz were all realised by combining
+                this <a
+                    href="https://www.gokapture.com/benefits-of-branded-games-for-exhibitions-and-trade-shows">interactive
+                    branded game</a> setup with a vending machine system that would dole out prizes, i.e., WOW’s very
+                own products, to the victors. The booth was lit by the genuine smiles and gratitude of cheerful
+                participants and onlookers, who were greatly vocal about the joyful moments spent at the booth, and also
+                took to social media to share their experience.
+                Photo collage:
+            </p>
+            <a
+                href="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaAvS58a4rlNcvdEsWyAze9as-YYlozgPOeRUFAvvxJqgwzcUa4vTK66ZcvjGpXC88t6vACIEovk77DjKkMiz_keBQddLg=w1920-h924">https://drive.google.com/file/d/1N5p5ajmwzCf-yYx9s-fy9kW_BUukeCW_/view</a>
+            <p>Alt text: interactive games for WOW at Nykaaland
+            </p>
+            <a
+                href="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaBdxm3KEmB2D2I8s8lSh6T7sCZMhGCL_FQ4KIy9Ct0S2CCIjwDQUBcx5EoYVSr_GJXfpsVhA5MVqMbn82nS53HPTr5E=w1920-h924">https://drive.google.com/file/d/1ZGlrnE8JTP5czawpTvASUfRE_6SnMzDF/view</a>
+
+            <p>Alt text: game-based vending machine for WOW at Nykaaland
+            </p>
+            <a
+                href="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCU1v_9n5KfmBo3bj-6h0QLaaKxSS5CYTCl_HPY_QK2PnYwCGszKyhDzlvP0X82nrWc4Deaa3wPIocGcuzzzhA6foGu=w1920-h924">https://drive.google.com/file/d/1r6JauR8cU6xrQK9mnox_22FYUqqC3gDt/view</a>
+            <p>Alt text: interactive game vending machine for WOW Skin Science by GoKapture
+            </p>
+            <p>
+                An interactive game-based vending machine makes for a head-turning means of promotions and <a
+                    href="https://www.gokapture.com/promotional-event">BTL marketing</a> at festivals, <a
+                    href="https://www.gokapture.com/exhibitions">exhibitions</a> and trade shows. Such a setup may be
+                configured to house a variety of <a href="https://www.gokapture.com/digital-touch-games">digital touch
+                    games</a> meant for branding and lead generation purposes. <a
+                    href="https://www.gokapture.com/games">Games and quizzes</a> are highly versatile, and can be
+                designed to embody any kind of occasion to a T. Not only were we rewarded with increased footfall, but
+                also heightened brand retention and invocation of positive feelings associated with the booth experience
+                with this customized one-on-one interaction.
+            </p>
+            <p>Check out the quality of our Interactive Game Vending Machine (Watch it in HD):
+            </p>
+            <div class="center">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/meEERgwzFvE?si=7Gn04yut3HTURbfG"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+            </div>
+            <p>Plan your next venture into event design and execution alongside GoKapture, and deliver to your audience
+                an utterly unforgettable experience. From masterful <a
+                    href="https://www.gokapture.com/gamification-in-the-exhibition-space">event gamification </a>with
+                leading technology such as <a href="https://www.gokapture.com/ar-vr">AR/VR</a> and <a
+                    href="https://www.gokapture.com/IoT">Internet of Things</a> to </p>
+            <p>capturing precious memories with the trendiest of <a
+                    href="https://www.gokapture.com/photo-booth-in-mumbai">photobooths in Mumbai</a> and beyond, such as
+                the <a href="https://www.gokapture.com/360-degree-photobooth">360 Video Booth</a>, ours is an
+                ever-expanding portfolio of realised visions. Reimagine your <a
+                    href="https://www.gokapture.com/interactive-games-in-pune-for-corporate-event.php">Company Day
+                    celebrations</a> with our custom solutions, or make your presence felt at galas such as the <a
+                    href="https://www.gokapture.com/selfie-booth-in-goa-for-fest">Goa Fest</a> or <a
+                    href="https://www.gokapture.com/strip-photo-booth-for-pet-festival-in-delhi-ncr">PetFed</a> with the
+                classic <a href="https://www.gokapture.com/selfie-photo-booth">Selfie Booth</a> and <a
+                    href="https://www.gokapture.com/strip-photo-booth">Strip Photo Booth</a>. Gift the attendees heading
+                your way uninhibited joy and lovely moments to share with friends!
+            </p>
+            <p>Call us to book: <b>9963702557, 8886666793</b></p>
+        </section>
+    </main>
+
+
+    <?php include("footer.php"); ?>
+
+
+    <!-- All JavaScript Files-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/default/classy-nav.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/default/jquery.scrollup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/default/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.animatedheadline.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jarallax.min.js"></script>
+    <script src="js/jarallax-video.min.js"></script>
+    <script src="js/default/scrollindicator.js"></script>
+    <script src="js/default/active.js"></script>
+</body>
+
+</html>
